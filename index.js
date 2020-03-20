504accff73cc5cd68787baabdc1afa4efdb41a6f
sam tags <stags.js@protonmail.com>
2020-03-21 04:39:36 +0800

    display - feature display ready to QA

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './resources/borders';
 export * from './resources/colors';
 export * from './resources/visibility';
 export * from './resources/position';
+export * from './resources/display';
