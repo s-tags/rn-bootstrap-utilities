91e04fcd4c0c645e49277c9d4253a296b57832bf
sam tags <stags.js@protonmail.com>
2020-03-21 06:17:02 +0800

    visibility - feature visibility ready to QA

---
+++ resources/visibility/index.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	visible: { opacity: 1 },
+	invisible: { opacity: 0 }
+});
+
+export const { visible, invisible } = styles;
