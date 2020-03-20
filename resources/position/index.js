ca06fa0b2182a554cb18b1d7130a19da24526cf0
sam tags <stags.js@protonmail.com>
2020-03-21 06:01:08 +0800

    positions - feature position to QA

---
+++ resources/position/index.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	positionAbsolute: { position: 'absolute' },
+	positionRelative: { position: 'relative' }
+});
+
+export const { positionAbsolute, positionRelative } = styles;
