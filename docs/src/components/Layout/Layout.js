6bf5a2bc4bbec7df134fb8bb723c5e4616682077
sam tags <stags.js@protonmail.com>
2020-03-22 18:46:21 +0800

    #14 - Intitialized Web page & Created Layout

---
+++ docs/src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import style from './index.module.scss';
+
+const Layout = () => {
+	return (
+		<div className={style.layout}>
+			<div
+				className={`
+        d-lg-flex flex-column d-none
+        ${style.desktop}
+      `}
+			>
+				<nav className="flex-shrink-0">Navbar</nav>
+				<div className="flex-grow-1 d-flex">
+					<aside className="flex-shrink-0">Aside</aside>
+					<div className="d-flex flex-grow-1">
+						<section className={`${style.content} flex-grow-1`}>
+							Main Content
+						</section>
+						<aside className="flex-shrink-0">Aside Right</aside>
+					</div>
+				</div>
+			</div>
+
+			<div className={`${style.mobile} d-block d-lg-none`}>
+				<nav>Navbar</nav>
+				<aside>Aside</aside>
+				<section className={style.content}>Main Content</section>
+			</div>
+		</div>
+	);
+};
+
+export default Layout;
