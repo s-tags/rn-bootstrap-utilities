4c977818d0be0dab52ef4aaa18980744a21be647
goni002 <tagalog808@gmail.com>
2020-04-01 19:35:02 +0800

    Created Hompage

--- docs/src/routes/index.js
+++ docs/src/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
+import Homepage from '../pages/Homepage/HomePage';
+
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
+			<Route exact component={Homepage} path="/" />
 				<Layout>
-					<Route path="/home">Content</Route>
+				<Route path="/home">Content</Route>
 				</Layout>
 			</Switch>
 		</BrowserRouter>
