1288773a4d6d4f5407fd3084e23e92c8768de883
stags <40300829+s-tags@users.noreply.github.com>
2020-09-18 18:53:49 +0800

    Colors add phone preview

--- docs/src/pages/Documentation/Utilities/Colors/Colors.js
+++ docs/src/pages/Documentation/Utilities/Colors/Colors.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Phone from '../../../../components/Phone/Phone';
 import style from './index.module.scss';
 
 function Colors() {
@@ -18,7 +19,7 @@ function Colors() {
 		'textMuted',
 		'textWhite',
 		'textBlack50',
-		'textWhite50'
+		'textWhite50',
 	];
 
 	const bg = [
@@ -34,10 +35,10 @@ function Colors() {
 		'bgMuted',
 		'bgWhite',
 		'bgBlack50',
-		'bgWhite50'
+		'bgWhite50',
 	];
 
-	colorsCode = colors.map(item => {
+	colorsCode = colors.map((item) => {
 		return (
 			<div>
 				<code className="text-info">{`<Text `}</code>
@@ -50,7 +51,7 @@ function Colors() {
 		);
 	});
 
-	bgCode = bg.map(item => {
+	bgCode = bg.map((item) => {
 		return (
 			<div>
 				<code className="text-info">{`<View `}</code>
@@ -74,20 +75,26 @@ function Colors() {
 			</p>
 
 			<header>Color</header>
-			<div className={`mt-2 border w-100 p-4 ${style.sampleContainer}`}>
-				<div className="text-primary mb-2">textPrimary</div>
-				<div className="text-secondary my-2">textSecondary</div>
-				<div className="text-success my-2">textSuccess</div>
-				<div className="text-danger my-2">textDanger</div>
-				<div className="text-warning my-2">textWarning</div>
-				<div className="text-info my-2">textInfo</div>
-				<div className="text-light bg-dark my-2">textLight</div>
-				<div className="text-dark my-2">textDark</div>
-				<div className="text-body my-2">textBody</div>
-				<div className="text-muted my-2">textMuted</div>
-				<div className="text-white bg-dark my-2">textWhite</div>
-				<div className="text-black-50 my-2">textBlack50</div>
-				<div className="text-white-50 bg-dark my-2">textWhite50</div>
+			<div
+				className={`mt-2 border w-100 p-4 d-flex justify-content-center ${style.sampleContainer}`}
+			>
+				<Phone>
+					<div className="w-100">
+						<div className="text-primary mb-2">textPrimary</div>
+						<div className="text-secondary my-2">textSecondary</div>
+						<div className="text-success my-2">textSuccess</div>
+						<div className="text-danger my-2">textDanger</div>
+						<div className="text-warning my-2">textWarning</div>
+						<div className="text-info my-2">textInfo</div>
+						<div className="text-light bg-dark my-2">textLight</div>
+						<div className="text-dark my-2">textDark</div>
+						<div className="text-body my-2">textBody</div>
+						<div className="text-muted my-2">textMuted</div>
+						<div className="text-white bg-dark my-2">textWhite</div>
+						<div className="text-black-50 my-2">textBlack50</div>
+						<div className="text-white-50 bg-dark my-2">textWhite50</div>
+					</div>
+				</Phone>
 			</div>
 			<div className={`${style.codeContainer} p-4`}>
 				<div className="text-right font-weight-normal text-secondary small">
@@ -98,17 +105,29 @@ function Colors() {
 			</div>
 
 			<header className="mt-3">Background color</header>
-			<div className={`mt-2 border w-100 p-4 ${style.sampleContainer}`}>
-				<div className="bg-primary p-2 mb-2 text-light">bgPrimary</div>
-				<div className="bg-secondary text-light p-2 my-2">bgSecondary</div>
-				<div className="bg-success text-light p-2 my-2">bgSuccess</div>
-				<div className="bg-danger text-light p-2 my-2">bgDanger</div>
-				<div className="bg-warning text-light p-2 my-2">bgWarning</div>
-				<div className="bg-info text-light p-2 my-2">bgInfo</div>
-				<div className="bg-light text-dark p-2 my-2">bgLight</div>
-				<div className="bg-dark text-white p-2 my-2">bgDark</div>
-				<div className="bg-white text-dark p-2 my-2">bgWhite</div>
-				<div className="bg-transparent text-dark p-2 my-2">bgTransparent</div>
+			<div
+				className={`mt-2 border w-100 p-4 d-flex justify-content-center	 ${style.sampleContainer}`}
+			>
+				<Phone>
+					<div className="w-100">
+						<div className="bg-primary p-1 mb-2 text-light">bgPrimary</div>
+						<div className="bg-secondary text-light p-1 my-2">bgSecondary</div>
+						<div className="bg-success text-light p-1 my-2">bgSuccess</div>
+						<div className="bg-danger text-light p-1 my-2">bgDanger</div>
+						<div className="bg-warning text-light p-1 my-2">bgWarning</div>
+						<div className="bg-info text-light p-1 my-2">bgInfo</div>
+						<div className="bg-light text-dark p-1 my-2">bgLight</div>
+						<div className="bg-dark text-white p-1 my-2">bgDark</div>
+						<div className="bg-white text-dark p-1 my-2">bgWhite</div>
+						<div
+							style={{ backgroundColor: '#00000073' }}
+							className="bg-black-50 text-dark p-1 my-2"
+						>
+							bgBlack50
+						</div>
+						<div className="bg-transparent text-dark p-1 my-2">bgWhite50</div>
+					</div>
+				</Phone>
 			</div>
 
 			<div className={`${style.codeContainer} p-4`}>
